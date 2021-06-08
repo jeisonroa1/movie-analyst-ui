cb3e855d7a414194151e301c1b569ebb3acf8467
Ivan Roa Mora <iroam@psl.com.co>
2021-06-07 21:43:47 -0500

    Adding JenkinsfileCI, Tests and Package.json updated. Checkout-Build-Test Stages Working.

--- test/test.js
+++ test/test.js
@@ -1,26 +1,59 @@
 process.env.NODE_ENV = "test";
+//var backendHost = process.env.BACK_HOST || 'localhost';
 
 //Require the dev-dependencies
 let chai = require("chai");
 let chaiHttp = require("chai-http");
-let server = require("../server");
+let expect = require("chai").expect;
 let should = chai.should();
+let server = require('../server');
+//let host = 'http://'+backendHost+':3030';
+
 
 chai.use(chaiHttp);
 //Our parent block
-describe("API", () => {
+describe("UI", () => {
   /*
    * Test the /GET route
    */
-  describe("/GET home", () => {
-    it("it should GET any reply", done => {
-      chai
-        .request(server)
-        .get("/")
-        .end((err, res) => {
-          res.should.have.status(200);
+  describe("GET home page", () => {
+    it("it should reply 200 over /", done => {
+    chai.request(server) 
+      .get('/')
+      .end(function(err, res){
+            expect(res.status).to.equal(200);
           done();
         });
-    });
+      });
+  });
+  describe("GET movies page", () => {
+    it("it should reply 200 over /movies", done => {
+    chai.request(server) 
+      .get('/movies')
+      .end(function(err, res){
+            expect(res.status).to.equal(200);
+          done();
+        });
+      });
+  });
+  describe("GET reviewers page", () => {
+    it("it should reply 200 over /authors", done => {
+    chai.request(server) 
+      .get('/authors')
+      .end(function(err, res){
+            expect(res.status).to.equal(200);
+          done();
+        });
+      });
+  });
+  describe("GET publications page", () => {
+    it("it should reply 200 over /publications", done => {
+    chai.request(server) 
+      .get('/publications')
+      .end(function(err, res){
+            expect(res.status).to.equal(200);
+          done();
+        });
+      });
   });
 });
